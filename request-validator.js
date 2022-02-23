016eade0ca082b5da61617e3607845b29cf0eb89
jordan.bispo <jordan.bispo@aeroscan.com.br>
2022-02-23 17:46:18 -0300

    partial implement

--- request-validator.js
+++ request-validator.js
@@ -2,6 +2,13 @@ module.exports = function (RED) {
     function RequestValidator(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+
+        this.editor = RED.editor.createEditor({
+            id: 'node-input-json-editor',
+            mode: 'ace/mode/text',
+            value: this.text
+        });
+
         node.on('input', function (msg) {
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
