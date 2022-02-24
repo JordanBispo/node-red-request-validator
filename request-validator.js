66d16a586b9ef4541bf546b34e29e019d5a8c2cb
JordanBispo <jordanbispo.dev@gmail.com>
2022-02-23 22:33:48 -0300

    version 0.1.0

--- request-validator.js
+++ request-validator.js
@@ -1,19 +1,82 @@
+
+const isBadRequest = (schema = {}, input = {}) => {
+    const schemaKeys = Object.keys(schema)
+    if (schemaKeys.length < 1) return false
+    for (let key of schemaKeys) {
+        if (Object.keys(input)?.includes(key)) {
+            if (typeof input[key] !== schema[key].type) {
+                return `${key} must be a ${schema[key].type}`
+            }
+        } else if (!!schema[key].required) {
+            return `${key} are required`
+        }
+    }
+    return false
+}
+
+const next = (msg) => {
+    if (msg['_payload']) msg['payload'] = msg['_payload']
+    return ([msg, null])
+}
+
+const reject = (msg, payload, httpStatus = 400) => {
+    msg['payload'] = payload
+    msg['statusCode'] = httpStatus
+    return [null, msg]
+}
+
 module.exports = function (RED) {
     function RequestValidator(config) {
         RED.nodes.createNode(this, config);
         var node = this;
 
         node.on('input', function (msg) {
-            msg['valid'] = config;
-            if (config.query) {
-                let query = JSON.parse(config.query);
-                let queryKeys = Object.keys(query);
-                let inputQuery = msg.req.query;
-                let inputQueryKeys = Object.keys(inputQuery);
 
+            try {
+
+                if (msg['payload']) msg['_payload'] = msg['payload']
+
+                if (config.method && config.method !== msg.req?.method) {
+                    node.send(reject(msg, { error: `Request method must be a ${config.method}` }))
+                    return
+                }
+
+                if (config.query) {
+
+                    let querySchema = JSON.parse(config.query);
+                    const validatorResult = isBadRequest(querySchema, msg.req?.query || {})
+
+                    if (!!validatorResult) {
+                        node.send(reject(msg, { error: validatorResult + ' in query' }))
+                        return
+                    }
+
+                }
+
+                if (config.reqbody) {
+
+                    let bodySchema = JSON.parse(config.reqbody);
+                    const validatorResult = isBadRequest(bodySchema, msg.req?.body || msg.payload)
+
+                    if (!!validatorResult) {
+                        node.send(reject(msg, { error: validatorResult + ' in body' }))
+                        return
+                    }
+
+                }
+
+                node.send(next(msg))
+                return
+
+            } catch (error) {
+                node.error("\nERROR on request-validation Node:");
+                node.error(error)
+                node.trace("\nError Trace")
+                node.send(reject(msg, { error: error }, 500))
             }
-            node.send(msg);
+
         });
+
     }
     RED.nodes.registerType("request-validator", RequestValidator, {
         settings: {
